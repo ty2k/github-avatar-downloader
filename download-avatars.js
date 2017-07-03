1984e615b7333f36c7a5def3fe076fffd84dc8f7
Tyler Krys <tylerkrys@gmail.com>
2017-07-03 23:01:03 +0000

    Adds getRepoContributors function skeleton and a call to it

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,16 @@
+// Require request
 var request = require('request');
 
+// Say hi to our user
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+// Create getRepoContributors function with arguments repoOwner (user), repoName (project), and the callback function
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+// Call getRepoContributors function with arguments jquery (user), jquery (project), and a function to log an error and results
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});

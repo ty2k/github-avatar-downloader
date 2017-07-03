f73a2bb5fe90c6d0b3f0abb78480531116adc562
Tyler Krys <tylerkrys@gmail.com>
2017-07-03 23:16:41 +0000

    Adds user and token variables, generates request URL

--- download-avatars.js
+++ download-avatars.js
@@ -6,7 +6,13 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 // Create getRepoContributors function with arguments repoOwner (user), repoName (project), and the callback function
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  // process.env.GITHUB_USER is any reference to your username; an environmental variable in this case
+  var GITHUB_USER = process.env.GITHUB_USER;
+  // process.env.GITHUB_ACCESS_TOKEN is any reference to your access token
+  var GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+  // Create our request URL using above variables
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 // Call getRepoContributors function with arguments jquery (user), jquery (project), and a function to log an error and results

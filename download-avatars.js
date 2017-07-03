19b233948ccb64a00c7c91fddb19a1dcc59dbd2f
Tyler Krys <tylerkrys@gmail.com>
2017-07-03 23:47:25 +0000

    Adds a requestOptions object to pass headers including User-Agent in our GET request

--- download-avatars.js
+++ download-avatars.js
@@ -12,7 +12,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
   // Create our request URL using above variables
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  // Create a requestOptions object that is able to pass headers, including User-Agent (we got a 403 status code if we leave this out)
+  var requestOptions = {
+      headers: {
+        'User-Agent': 'GitHub Avatar Downloader Exercise'
+      }
+  }
+
+  // Use request to make an HTTP GET to the generated URL
+  request(requestURL, requestOptions, function(err, response, body) {
+    if (err) {
+      throw err;
+    }
+    console.log('Response body:', response.body);
+  });
 }
 
 // Call getRepoContributors function with arguments jquery (user), jquery (project), and a function to log an error and results
